8c076d4263d9519026a5ef3cadb2a9866f80b752
datapolitan <richard@datapolitan.com>
2019-06-11 22:47:54 +0000

    adding github setup with rsa keys

--- git_setup.sh
+++ git_setup.sh
@@ -2,3 +2,6 @@
 # based on https://bren.zendesk.com/hc/en-us/articles/360015826731-How-to-connect-RStudio-Cloud-with-Github
 git config --global user.email "your email" #enter your Github email here
 git config --global user.name "your name" # enter your Github username here
+
+# configure the github connection after setting the rsa key
+git config remote.origin.url git@github.com:datapolitan/getting-started.git
