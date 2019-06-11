d8bcbe266252c15de9e1fe222cfa54ec824850d0
datapolitan <richard@datapolitan.com>
2019-06-11 22:37:08 +0000

    Adding setup file for github

---
+++ git_setup.sh
@@ -0,0 +1,4 @@
+# Introduction/setup in Terminal window (not console)
+# based on https://bren.zendesk.com/hc/en-us/articles/360015826731-How-to-connect-RStudio-Cloud-with-Github
+git config --global user.email "your email" #enter your Github email here
+git config --global user.name "your name" # enter your Github username here
