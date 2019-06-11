35054a7e4cf7f353af06e21e285edce2eac7487c
datapolitan <richard@datapolitan.com>
2019-06-11 23:00:12 +0000

    Adding getting started R tutorial text

---
+++ GettingStarted.R
@@ -0,0 +1,17 @@
+# Getting Started
+
+# The "#" character tells R that this line is a comment
+# Comments are meant for human users, not computers. They just ignore comments
+# I'm using comments in this file to give instructions and clarify points
+# You should think about using comments in your code to help explain operations and code blocks
+# This is how we remind ourselves and tell others of our work in the code itself
+
+
+# Put any type of basic mathematial operation (1+2, etc.)
+
+
+# Run the code above by clicking the "Run" button above 
+# or clicking "Cmd + Enter" (on Mac) or "CTRL + Enter" (on Windows)
+# More on Running code in R from a script here: 
+
+
